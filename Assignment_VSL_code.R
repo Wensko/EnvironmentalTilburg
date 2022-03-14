897efa8d00a1e7aefed7cace977b40b285891e6a
Alexandre Wensko <awensko@uol.com.br>
2022-03-14 17:10:23 +0100

    Here, I've commited til the first 5 tasks. I still intend to plot some graphs for them, so we can add in the paper.

--- Assignment_VSL_code.R
+++ Assignment_VSL_code.R
@@ -7,47 +7,86 @@ library(tidyverse)
 library(ggdag)
 library(dplyr)
 library(haven)
+library(stargazer)
 
 
 # read in data file
-VSL_Project <- read_dta("YOUR FOLDER PATH/ProjectVSL_Data.dta")
+VSL_Project <- read_dta("ProjectVSL_Data.dta")
 
 # generate dummy variable "old", where old is defined as all workers above the age of 45
     VSL_Project$old <- ifelse(VSL_Project$age > 45, 1, 0)
 
 # summary stats of risk levels; see Table 1 of Gegax at al. (1991).
-   # Examples:  
-    sample_union_blue <- filter(VSL_Project, union == 1 & blue == 1)  # defines the subsample of blue-collar workers who are member of a labor union
-    summary(sample_union_blue$risk) # calculates average risk unionized blue-collar workers face in their jobs
+# 'union.blue', 'union.white', 'nunion.blue', 'nunion.white', 'all.union', 'all.nunion', 'white', 'blue', 'full')
 
-    meanrisk_union_blue <- round(mean(sample_union_blue$risk),digits = 3) # To be used later in Table to beconstructed for Task 4.
-    summary(meanrisk_union_blue)
+    ##CREATING SUBSAMPLES
+    union.blue <- filter(VSL_Project, union == 1 & blue == 1)
+    union.white <- filter(VSL_Project, union == 1 & blue == 0)
+    nunion.blue <- filter(VSL_Project, union == 0 & blue == 1)  
+    nunion.white <- filter(VSL_Project, union == 0 & blue == 0)
+    all.union <- filter(VSL_Project, union == 1)
+    all.nunion <- filter(VSL_Project, union == 0)
+    white <- filter(VSL_Project, blue == 0)
+    blue <- filter(VSL_Project, blue == 1)
+    full <- VSL_Project
     
-    sample_union_all <- filter(VSL_Project, union == 1)  # defines the subsample of all workers who are member of a labor union
-    summary(sample_union_all$risk)
-    meanrisk_union_all <- round(mean(sample_union_all$risk),digits = 3)
-    summary(meanrisk_union_all)
+    a <- c('union.blue', 'union.white', 'nunion.blue', 'nunion.white', 'all.union', 'all.nunion', 'white', 'blue', 'full')
+    b <- c()
+    c <- c()
+    d <- c()
+    e <- c()
+    ## PUT ALL SAMPLS IN A LIST SO I CAN LOOP
+    base <- list(union.blue, union.white, nunion.blue, nunion.white, all.union, all.nunion, white, blue, full)
+    names(base) <- a
     
-    # Task 1A: The above code calculates the means, but not standard deviations. Adjust the code. 1B. Replicate the remainder of Table 1, preferably using the type of code to construct a table as presented for the VSL calculations; see lines 86-91.   
-
-    
-# Who selects into risky jobs?
-      reg1_1 <- lm(risk ~ age + race + sex + disab + vet + live # The personal characteristics indicators
-                  , data = VSL_Project)
-      summary(reg1_1)
-    
-    # Task 2A. Do "older people" select in the safer jobs? Replace "age" by "old", and interpret the results. 2B. Using a similar regression but a different dependent variable, who selects into unionized jobs? 2C. And are unionized jobs more risky that non-unionized jobs? (Hint: for 2C, run a t-test).
-
-
-# There are three sets of worker and job characteristics
-  # The human capital indicators are: SCHOOL1 SCHOOL2 SCHOOL3 SCHOOL4 SCHOOL5 yrspo yrsft yrspe
-    #SCHOOL6 is the omitted category; when running the regressions, this variable does NOT have to be included
-  # The indicators of the work environment are: RQSCHL1 RQSCHL2 RQSCHL3 RQSCHL4 wkexp super govt union yrsqual miles number central service trans labor equip craft cleric sales manage
-    #RQSCHL5 and PROF are the omitted categories; when running the regressions, these variables do NOT have to be included
-  # The indicators of the personal characteristics are: age race sex disab vet live
-    # For this type of characteristics there are no omitted categories
-# If you use any of these worker or job characteristics indicators in a regression, always include ALL indicators that belong to the set 
+    for (i in 1:length(base)) {
+      b <- c(b,nrow(base[[i]]))
+      c <- c(c, round(mean(base[[i]]$risk), digits = 3))
+      d <- c(d, round(sd(base[[i]]$risk), digits = 3))
+      e <- c(e, round(mean(base[[i]]$fatal), digits = 3))
       
+    }
+    #TABLE FOR SUMMARY STATS
+tabela1 <- cbind(a,b,c,d)
+colnames(tabela1) <- c('subsample', 'N', 'Risk', 'SD')
+stargazer(tabela1, type = 'text')
+
+# Who selects into risky jobs?
+reg1_1 <- lm(risk ~ age + race + sex + disab + vet + live # The personal characteristics indicators
+                  , data = VSL_Project)
+summary(reg1_1)
+
+## FROM HERE, IT SEEMS MEN AND PEOPLE WHO DOESN'T LIVE IN CENTRAL CENTERS SELECT INTO RISKIER JOBS.
+#  NEVERTHELESS, WE GOTTA TAKE CARE OF ENDOGENEITY AND OMITTED VARIABLE BIAS
+# AS LIVIN IN THE SUBURBS MIGHT RESTRICT WHAT KINDS OF JOBS ARE AVAILABLE TO YOU, FOR EXAMPLE. OLDER
+# PEOPLE ALSO SEEM TO AVOID RISKY. 
+
+    
+# Task 2A. Do "older people" select in the safer jobs? Replace "age" by "old", and interpret the results. 
+# 2B. Using a similar regression but a different dependent variable, who selects into unionized jobs? 
+# 2C. And are unionized jobs more risky that non-unionized jobs? (Hint: for 2C, run a t-test).
+
+reg2_A <- lm(risk ~ old + race + sex + disab + vet + live # The personal characteristics indicators
+             , data = VSL_Project)
+summary(reg2_A)
+## WE NOW HAVE SIMILAR RESULTS. THE ONLY THING WE GOTTA STRESS IS THE FACT THAT NOW, USING A DUMMY, WE BE 
+## ESTIMATING CONDICIONAL MEANS TO BINARY VARIABLES (THERE ISN'T ANY CONTINUOUS VARIABLE)
+
+reg2_B <- lm(union ~ risk + race + sex + disab + vet + live # The personal characteristics indicators
+             , data = VSL_Project)
+summary(reg2_B)
+
+# IN THIS CASE, IT SEEMS THAT VERERANS AND THOSE WHO WORK INDO RISKIER OCCUPATIONS PARTICIPATE IN UNIONS
+# MORE. AGAIN, WE GOTTA PAY ATTENTION TO ENDOGENEITY. IT MIGHT BE THAT UNIONS ARE MORE PRESENT IN RISKIER
+# OCCUPATIONS TO FULFIL BIGGER DEMAND BY THE WORKERS. IT MIGHT ALSO BE THE CASE, AS STRESSED BY THE PAPER
+# THAT UNIONS RAISE AWARENESS REGARDING RISKS, AND THEN THIS CAUSE A ELEVATED PERCEPTION OF RISK IN UNIONIZED
+# WORKERS.
+
+test2_c <- t.test(all.union$risk, all.nunion$risk)
+
+# ACCORDING TO A SIMPLE T-TEST ON MEANS, WE CAN REJECT THE HYPOTHESIS THAT THE RISK IN THE TWO 
+# STRATS ARE THE SAME. INDEED, THE RISK IS GREATER AT UNIONIZED JOBS (MEAN 3.442482)
+
       
 # This regression replicates Table 2
       reg2 <- lm(lnrwage ~ risk + 
@@ -57,53 +96,121 @@ VSL_Project <- read_dta("YOUR FOLDER PATH/ProjectVSL_Data.dta")
                    , data = VSL_Project)
       summary(reg2)
 
-
-# This replicates the first result of Table 3
-      reg3_1 <- lm(lnrwage ~ risk + 
-                   SCHOOL1 + SCHOOL2 + SCHOOL3 + SCHOOL4 + SCHOOL5 + yrspo + yrsft + yrspe + # The human capital indicators
-                   RQSCHL1 + RQSCHL2 + RQSCHL3 + RQSCHL4 + wkexp + super + govt + union + yrsqual + miles + number + central + service + trans + labor + equip + craft + cleric + sales + manage + # The indicators of the work environment
-                   age + race + sex + disab + vet + live # The personal characteristics indicators
-                   , data = sample_union_blue)
-      summary(reg3_1)
+      # Task 3: Replicate the remainder of Table 3, and interpret the results. Preferable present the results in a Table using a code similar to the code written to produce a table for Task 4 (lines 86-91).
       
-      riskcoef_union_blue <- round(coef(summary(reg3_1))["risk","Estimate"], digit = 4) # store coefficient with 4 decimals; needed to calculate VSL later on
-      summary(riskcoef_union_blue)
+      
+      ## I CREATED 2 LISTS, SO I CAN LOOP FOR THE VARIABLE RISK AND FOR THE VARIABLE FATAL
+      regs3_risk <- list()
+      regs3_fatal <- list()
+      # IN THIS LOOP, WE CALCULATE THE REGRESSIONS OF WAGE IN RISK/FATAL AND THE OTHER COVARIATES
+      for (i in 1:length(base)) {
+        regs3_risk[[i]] <- lm(lnrwage ~ risk + 
+                      SCHOOL1 + SCHOOL2 + SCHOOL3 + SCHOOL4 + SCHOOL5 + yrspo + yrsft + yrspe + # The human capital indicators
+                      RQSCHL1 + RQSCHL2 + RQSCHL3 + RQSCHL4 + wkexp + super + govt + union + yrsqual + miles + number + central + service + trans + labor + equip + craft + cleric + sales + manage + # The indicators of the work environment
+                      age + race + sex + disab + vet + live # The personal characteristics indicators
+                      , data = base[[i]])
+        regs3_fatal[[i]] <- lm(lnrwage ~ fatal + 
+                                SCHOOL1 + SCHOOL2 + SCHOOL3 + SCHOOL4 + SCHOOL5 + yrspo + yrsft + yrspe + # The human capital indicators
+                                RQSCHL1 + RQSCHL2 + RQSCHL3 + RQSCHL4 + wkexp + super + govt + union + yrsqual + miles + number + central + service + trans + labor + equip + craft + cleric + sales + manage + # The indicators of the work environment
+                                age + race + sex + disab + vet + live # The personal characteristics indicators
+                              , data = base[[i]])
+        
+      }
+      
 
-  # Task 3: Replicate the remainder of Table 3, and interpret the results. Preferable present the results in a Table using a code similar to the code written to produce a table for Task 4 (lines 86-91).
-
-  # Task 4: For all those subsamples for which the coefficient on risk was signifIcant, calculate the VSL in three steps:
+      
+      ## IN THIS SECTION, WE EXTRACT THE ESTIMATES AND T-VALUES FROM THE REGRESSIONS TO CREATE TABLE 3
+      
+      tabela.risk <- data.frame(NA)
+      for (i in 1:9) {
+        tabela.risk[i,1] <- a[i]
+        tabela.risk[i,2] <- as.numeric(round(coef(summary(regs3_risk[[i]]))['risk','Estimate'], digits = 4))
+        tabela.risk[i,3] <- as.numeric(round(coef(summary(regs3_risk[[i]]))['risk','t value'], digits = 4))
+        
+        # Task 4: For all those subsamples for which the coefficient on risk was signifIcant, calculate the VSL in three steps:
         # Step 1:Calculate the mean ANNUAL income level (= wage rate x 2000, as wage rate is hourly, and a yr consists of 2000 working hours)
         # Step 2: Calculate the VSL 4000 X risk coefficient of the subsample x the subsample's mean annual income (as the risk is the number of deaths per 4000 workers)
+        
+        tabela.risk[i,4] <- 2000*round(mean(base[[i]]$wagerate), digits = 2)
+        tabela.risk[i,5] <- c[i]
+        tabela.risk[i,6] <- 4000*tabela.risk[i,2]*tabela.risk[i,4]
+      }
+      colnames(tabela.risk) <- c('subsamples', 'coefficient', 't-stat', 'Mean Annual Wage Rate', 'Mean Perceived Risk', 'VSL')
+      tabela.fatal <- data.frame(NA)
+      for (i in 1:9) {
+        tabela.fatal[i,1] <- a[i]
+        tabela.fatal[i,2] <- as.numeric(round(coef(summary(regs3_fatal[[i]]))['fatal','Estimate'], digits = 4))
+        tabela.fatal[i,3] <- as.numeric(round(coef(summary(regs3_fatal[[i]]))['fatal','t value'], digits = 4))
+        
+        # Task 4: For all those subsamples for which the coefficient on risk was signifIcant, calculate the VSL in three steps:
+        # Step 1:Calculate the mean ANNUAL income level (= wage rate x 2000, as wage rate is hourly, and a yr consists of 2000 working hours)
+        # Step 2: Calculate the VSL 4000 X risk coefficient of the subsample x the subsample's mean annual income (as the risk is the number of deaths per 4000 workers)
+        
+        tabela.fatal[i,4] <- 2000*round(mean(base[[i]]$wagerate), digits = 2)
+        tabela.fatal[i,5] <- e[i]
+        tabela.fatal[i,6] <- 1000*tabela.fatal[i,2]*tabela.fatal[i,4]
+      }
+      colnames(tabela.fatal) <- c('subsamples', 'coefficient', 't-stat', 'Mean Annual Wage Rate', 'Mean Fatal', 'VSL')
       
-      # Example:
-          # VSL_union_blue
-            summary(sample_union_blue$wagerate)    
-            meanannualwagerate_union_blue <- 2000 * round(mean(sample_union_blue$wagerate),digits = 2) # Measures the mean wage rate of the subsample of interest
-            summary(meanannualwagerate_union_blue)
-            VSL_union_blue <- round(4000 * riskcoef_union_blue * meanannualwagerate_union_blue, digits = 0) #Calculates the VSL
-            summary(VSL_union_blue) 
+      ## THE OBJECTS 'tabela.x' ARE TABLES CONTAININ NOT ONLY THE TABLE 3 BUT ALSO SOME THINGS THAT WILL BE REQUIRED FOR TASK 4
+      # HERE EXTRACT FROM 'tabela.x' the data for table 3
+      table3 <- cbind(tabela.risk[,1:3], tabela.fatal[2:3])
 
-      # VSL result summarized in a Table:
-          tab_VSL <- matrix(c(meanrisk_union_blue,VSL_union_blue), ncol=2, byrow=TRUE)
-          colnames(tab_VSL) <- c('Perceived Risk','VSL')
-          rownames(tab_VSL) <- c('Union Blue')
-          tab_VSL <- as.table(tab_VSL)
-          tab_VSL
+      # Task 4: For all those subsamples for which the coefficient on risk was signifIcant, calculate the VSL in three steps:
+      # Step 1:Calculate the mean ANNUAL income level (= wage rate x 2000, as wage rate is hourly, and a yr consists of 2000 working hours)
+      # Step 2: Calculate the VSL 4000 X risk coefficient of the subsample x the subsample's mean annual income (as the risk is the number of deaths per 4000 workers)
+      
+      ## Subsamples who present significant coeficients in table 3
+      # summary(regs3_risk[[1]]) #Union Blue
+      # summary(regs3_risk[[5]])  #all union
+      # summary(regs3_risk[[8]])  #blue
+      # summary(regs3_fatal[[1]])
+      # summary(regs3_fatal[[5]])
+      # summary(regs3_fatal[[8]])
+      
+      # USING 'tabela.x' WE SOLVE TASK 4
+      task4.table <- cbind(tabela.risk[c(1,5,8),c(1,4:6)], tabela.fatal[c(1,5,8),4:6]) 
 
 # Are some worker types (age (or old), race, sex, disab, vet, live) more averse to risk (= have a higher MWTP) than others?
-      reg5_1 <- lm(lnrwage ~ risk  + risk*old +
-                   SCHOOL1 + SCHOOL2 + SCHOOL3 + SCHOOL4 + SCHOOL5 + yrspo + yrsft + yrspe + # The human capital indicators
+
+      # AS SUGGESTED BY THE PROFESSOR, WE USE THE INTERACTION BETWEEN TYPES AND RISK TO CALCULATE IF
+      # ANY TYPE IS ABLE TO EXTRACT HETEROGENOUS PREMIUMS FROM RISKS
+      # THESE LISTS ARE GONNA STORE THE REGRESSIONS FOR EACH SAMPLE (BLUE.UNION AND ALL.UNION)
+      # AND EACH TYPE (old, race, sex, disab, vet, live)
+      task5_union.blue.regs <- list()
+      task5_all.union.regs <- list()
+      
+      #WE GONNA USE THIS INSIDE THE LOOP
+      chars <- c('old', 'race', 'sex', 'disab', 'vet', 'live')
+      
+      # THESE DATAFRAMES WERE CREATED SO WE CAN STORE THE VALUES FOR ESTIMATES OF THE INTERACTION
+      # TERMS AND THEIR Pr(>|t|). 
+      
+      task5_union.blue.estimates <- data.frame(Characteristics = chars, Estimate = NA, pvalue = NA)
+      task5_all.union.estimates <- data.frame(Characteristics = chars, Estimate = NA, pvalue = NA)
+      for (i in 1:6) {
+        formula.reg <- paste('lnrwage ~ risk  + risk*', chars[i],'+ SCHOOL1 + SCHOOL2 + SCHOOL3 + SCHOOL4 + SCHOOL5 + yrspo + yrsft + yrspe + # The human capital indicators
                    RQSCHL1 + RQSCHL2 + RQSCHL3 + RQSCHL4 + wkexp + super + govt + union + yrsqual + miles + number + central + service + trans + labor + equip + craft + cleric + sales + manage + # The indicators of the work environment
-                   age + race + sex + disab + vet + live # The personal characteristics indicators
-                 , data = VSL_Project)
-      summary(reg5_1)
+                   age + race + sex + disab + vet + live')
+        task5_union.blue.regs[[i]] <- lm(formula = formula.reg, data=union.blue)
+        task5_all.union.regs[[i]] <- lm(formula = formula.reg, data = all.union)
+        task5_union.blue.estimates[i,2] <- round(coef(summary(task5_union.blue.regs[[i]]))[paste('risk', chars[i], sep = ':'), 'Estimate'], digits = 4)
+        task5_all.union.estimates[i,2] <- round(coef(summary(task5_all.union.regs[[i]]))[paste('risk', chars[i], sep = ':'), 'Estimate'], digits = 4)
+        task5_union.blue.estimates[i,3] <- round(coef(summary(task5_union.blue.regs[[i]]))[paste('risk', chars[i], sep = ':'), 'Pr(>|t|)'], digits = 4)
+        task5_all.union.estimates[i,3] <- round(coef(summary(task5_all.union.regs[[i]]))[paste('risk', chars[i], sep = ':'), 'Pr(>|t|)'], digits = 4)
+      }
+      stargazer(task5_all.union.estimates, task5_union.blue.estimates,summary = F, type = 'text')
+      
+      # NONE OF THE TERMS GOT ANY STATISTICAL SIFNIFICANCE
 
-    # Task 5: 5A. Do the same for the subset of blue-collar workers who are member of a labor union. 5B. Do the same analysis for the other worker characteristics as defined on line 47, using the subset of union members. 
 
-
-
+      
+      ### I STILL HAVEN'T DONE 6 AND 7. I ALSO INTEND TO PLOT SOME GRAPHS SO WE CAN PUT THEM IN THE PAPER
+      
 # Task 6: 6A. What about elderly males -- do they have a lower VSL than elderly females, or than younger males? (Hint: use "old"!, and do not just calculate the MWTP, but also their VSL (see Task 4)).
         # 6B. Do public sector (government) employees tend to have a lower VSL than private sector (non-government) employees?
 
 
 # Task 7: write up the results of all Tasks concisely in a short paper, where you try to interpret the results. When looking at all results, what have you learnt about the VSL? Can it be measured using this technique, does it vary with personal characteristics as expected, and do the persons with the highest tolerance to risk select into the more risky jobs? 
+      
+      
